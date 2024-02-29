31e5e7f312d87ebd677b2446468a1f28c6659f77
이원형 <67450169+whl0526@users.noreply.github.com>
2024-02-29 16:29:43 +0900

    Initial Commit

---
+++ Beats/BeatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeatsApp.swift
+//  Beats
+//
+//  Created by 이원형 on 2/29/24.
+//
+
+import SwiftUI
+
+@main
+struct BeatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
