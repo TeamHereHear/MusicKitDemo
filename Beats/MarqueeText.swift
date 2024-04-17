813784d94f05498fcbab77e879422d5289c92e3b
이원형 <67450169+whl0526@users.noreply.github.com>
2024-04-17 10:58:11 +0900

    MusicKit Demo

---
+++ Beats/MarqueeText.swift
@@ -0,0 +1,18 @@
+//
+//  MarqueeText.swift
+//  Beats
+//
+//  Created by 이원형 on 3/1/24.
+//
+
+import SwiftUI
+
+struct MarqueeText: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MarqueeText()
+}
---
+++ Beats/SearchViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewModel.swift
+//  Beats
+//
+//  Created by 이원형 on 3/2/24.
+//
+
+import SwiftUI
+
+struct SearchViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchViewModel()
+}
